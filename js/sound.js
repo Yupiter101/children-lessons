f3c2e06b349f2dd7d70e252e2259b4ddffa741a8
Sergey_Sanyuk <shv08@ukr.net>
2025-01-29 01:19:11 +0200

    audio

--- js/sound.js
+++ js/sound.js
@@ -1,45 +1,80 @@
 console.log("Hello from audio js");
 
 
+
+const myPlayer = document.querySelector(".my-audio"); // Player
+
 const playBtn = document.querySelector("#play");
+const pauseBtn = document.querySelector("#pause");
 const stopBtn = document.querySelector("#stop");
 const playrBox = document.querySelector("#player-container");
-console.log(playrBox);
 
 
-const titleTeg = document.createElement("h2");
-titleTeg.textContent = "createdElement";
-playrBox.append(titleTeg);
-
-playBtn.addEventListener("click", onPlay);
-stopBtn.addEventListener("click", onStop);
 
 
-// // Завантаження звуків
+// Завантаження звуків
+
 const soundCrash = new Audio();
 soundCrash.src = "../sounds/crash1.wav";
-console.log(soundCrash);
 
-// playrBox.append(soundCrash);
-
-// // const musicGame = new Audio();
-// // musicGame.src = "../../sounds/music1.ogg";
+// const myMusic = new Audio();
+// myMusic.src = "../sounds/music1.ogg";
 
 
-// soundCrash.play();
-
-// soundCrash.pause();
-// soundCrash.currentTime = 0; 
 
 
+// ========== Слухачі =============
+
+playBtn.addEventListener("click", onPlay);
+pauseBtn.addEventListener("click", onPause);
+stopBtn.addEventListener("click", onStop);
+// myPlayer.addEventListener("timeupdate", onUpdateTime);
+
+
+
+// ============== FUNCTIONS ==================
 
 function onPlay() {
     console.log("play");
-    soundCrash.play();
+    myPlayer.play();
+    console.log(myPlayer.duration);
+    console.log(myPlayer.volume);
+    // myPlayer.duration
+    // soundCrash.addEventListener("playing", ()=> console.log("Audio started"));
+    // soundCrash.addEventListener("ended", ()=> console.log("Audio stoped"));
+}
+
+function onPause() {
+    console.log("Pause");
+    myPlayer.pause();
 }
 
 function onStop() {
     console.log("Stop");
-    soundCrash.pause();
-    soundCrash.currentTime = 0;
+    myPlayer.pause();
+    myPlayer.currentTime = 0;
 }
+
+function onUpdateTime(event) {
+    const { duration, currentTime } = event.srcElement;
+    console.log(duration, currentTime );
+}
+
+
+
+
+
+
+
+
+
+
+// ====== DRAFT ================
+
+// const titleTeg = document.createElement("h2");
+// titleTeg.textContent = "createdElement";
+// playrBox.append(titleTeg);
+
+
+   // soundCrash.addEventListener("playing", ()=> console.log("Audio started"));
+    // soundCrash.addEventListener("ended", ()=> console.log("Audio stoped"));
